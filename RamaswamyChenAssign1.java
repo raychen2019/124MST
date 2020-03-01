ea9bc608efdff82194b5aab723060ac46ccdf912
raychen2019 <55036528+raychen2019@users.noreply.github.com>
2020-02-29 21:07:35 -0500

    More correct, and includes generate threshold

--- RamaswamyChenAssign1.java
+++ RamaswamyChenAssign1.java
@@ -6,9 +6,9 @@ public class RamaswamyChenAssign1 {
 
 	static Random generator;
 	static double max = 0.0;
-	
+
 	public static void main(String[] args){
-		
+
 		int seedNum = Integer.parseInt(args[0]);
 		int numPoints = Integer.parseInt(args[1]);
 		int numTrials = Integer.parseInt(args[2]);
@@ -17,61 +17,77 @@ public class RamaswamyChenAssign1 {
 		generator = new Random(seedNum);
 		
 		float totalWeight = 0;
-		
+
 		for (int i = 0; i < numTrials; i++){
 			float MST = FindMST(numPoints, dimension);
 			totalWeight += MST;
 		}
 		System.out.println(max);
-//		System.out.println(totalWeight);
-		
-		
+//		System.out.println(totalWeight);		
+
 		System.out.print(totalWeight/numTrials);
+
+	}
+
+	
+
+	public static float generateWeight (Node one, Node two){
+
+		float weightTemp = 0;
+		for (int i = 0 ; i < one.getDim() ; i++)
+			weightTemp += Math.pow((one.getCoords()[i] - two.getCoords()[i]),2);
+		return (float) Math.sqrt(weightTemp);
+	}
+
+	public static float generateThreshold (int nodeNum, int dimension){
+		if (dimension == 1)
+			return (float) (4.91*Math.pow(nodeNum,-0.905)+.05);
+		else if (dimension == 2)
+			return (float) (2.24*Math.pow(nodeNum,-0.506)+.05);
+		else if (dimension == 3)
+			return (float) (1.49*Math.pow(nodeNum,-0.318)+.05);
+		else if (dimension == 4)
+			return (float) (1.54*Math.pow(nodeNum,-0.25)+.05);
 		
+		return (float) (Math.sqrt(dimension)/2);
 	}
 	
-	public static float generateWeight (int dimension){
-		
-		float weightTemp = 0;
-		for (int i = 0 ; i < dimension ; i++)
-			weightTemp += Math.pow((generator.nextFloat() - generator.nextFloat()),2);
-		
-		return (float) Math.sqrt(weightTemp); 
-	}
 	public static float FindMST (int numPoints, int numDimensions){
-
-		float treeWeight = 0;
 		
+		float treeWeight = 0;
+
 		//each vertex can be labeled as an integer
+
 		boolean[] visited = new boolean[numPoints];
 		visited[0] = true;
-		
+		Node[] nodes = new Node[numPoints];
+		for(int i = 0; i < numPoints; i++) {
+			nodes[i] = new Node(numDimensions, i);
+		}
+
 		PriorityQueue<Edge> heap = new PriorityQueue<Edge>();
-		
-		
+
 		for (int i = 1; i < numPoints; i++)
 		{
-			heap.add(new Edge (generateWeight (numDimensions), i));
+			heap.add(new Edge (generateWeight (nodes[0], nodes[i]), nodes[i]));
 		}
-		
-		
+
 		//the program terminates when we add n-1 edges 
 		int edgeCount = 0;
-		while (edgeCount < numPoints-1){
+		while (edgeCount < numPoints-1 && !heap.isEmpty()){
 			Edge temp = heap.poll();
-			
-			if (!visited[temp.getSndVertex()]) {
+			if (!visited[temp.getSndVertex().getIndex()]) {
 				if (max < temp.getWeight())
-					max = temp.getWeight();
+						max = temp.getWeight();
 				treeWeight += temp.weight;
-				visited[temp.getSndVertex()] = true;
-				
+				visited[temp.getSndVertex().getIndex()] = true;
+
 				for (int i = 1; i < numPoints; i++){
 					if (!visited[i])
 					{
-						float tempWeight = generateWeight(numDimensions);
-						if (tempWeight < 0.0065)
-							heap.add(new Edge (tempWeight, i));
+						float tempWeight = generateWeight(temp.getSndVertex(), nodes[i]);
+						if (tempWeight < generateThreshold(numPoints, numDimensions))
+							heap.add(new Edge (tempWeight, nodes[i]));
 					}
 				}
 				
@@ -88,9 +104,9 @@ public class RamaswamyChenAssign1 {
 	
 	static class Edge implements Comparable{
 		float weight;
-		int sndVertex;
+		Node sndVertex;
 		
-		public Edge (float weightParam, int sndVertexParam){
+		public Edge (float weightParam, Node sndVertexParam){
 			weight = weightParam;
 			sndVertex = sndVertexParam;
 		}
@@ -99,7 +115,7 @@ public class RamaswamyChenAssign1 {
 			return weight;
 		}
 		
-		public int getSndVertex(){
+		public Node getSndVertex(){
 			return sndVertex;
 		}
 
@@ -111,6 +127,33 @@ public class RamaswamyChenAssign1 {
 		}
 
 	}
+	
+	static class Node{
+		int dim;
+		float[] coords;
+		int index;
+		
+		public Node (int dimensions, int ind){
+			index = ind;
+			dim = dimensions;
+			coords = new float[dim];
+			for (int i = 0; i < dim; i++) {
+				coords[i] = generator.nextFloat();
+			}
+		}
+		
+		public int getIndex(){
+			return index;
+		}
+		
+		public int getDim(){
+			return dim;
+		}
+		
+		public float[] getCoords(){
+			return coords;
+		}
+
+	}
+	
 }
-
-
